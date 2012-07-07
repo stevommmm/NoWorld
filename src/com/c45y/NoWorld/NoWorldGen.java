ffe33043af4c6b4d00dc23d87024613b79cb7e43
Stephen Mcgregor <tehborris@gmail.com>
2012-07-07 12:23:55 +1000

    Nice dirt spheres

--- src/com/c45y/NoWorld/NoWorldGen.java
+++ src/com/c45y/NoWorld/NoWorldGen.java
@@ -2,6 +2,7 @@ package com.c45y.NoWorld;
 
 import java.util.Random;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.World;
 
@@ -16,35 +17,38 @@ public class NoWorldGen extends ChunkGenerator
 	public boolean canSpawn(World world, int x, int z) {
 		return true;
 	}
-	
-	public byte[][] generateBlockSections(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomeGrid)
-    {
-        byte[][] result = new byte[256 / 16][]; //world height / chunk part height (=16, look above)
-        for(int x = 0; x < 16; x++)
-        {
-            for(int z = 0; z < 16; z++)
-            {
-            	if (distance(x,0,z) < 8) {
-            		setBlock(result, x, 0, z, (byte) 20);
-            	}
-            }
-        }
-        return result;
-    }
 
-	void setBlock(byte[][] result, int x, int y, int z, byte blkid) {
-	    if (result[y >> 4] == null) //is this chunkpart already initialised?
-	    {
-	        result[y >> 4] = new byte[4096]; //initialise the chunk part
-	    }
-	    result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blkid; //set the block (look above, how this is done)
+	public byte[][] generateBlockSections(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomeGrid)
+	{
+		byte[][] result = new byte[256 / 16][]; //world height / chunk part height (=16, look above)
+		for(int y = 0; y < 16; y++) {
+			for(int x = 0; x < 16; x++)
+			{
+				for(int z = 0; z < 16; z++)
+				{
+					if (distance(x,y,z) < 8) {
+						setBlock(result, x, y, z, (byte) Material.DIRT.getId());
+					}
+				}
+			}
+		}
+		return result;
 	}
 
+	void setBlock(byte[][] result, int x, int y, int z, byte blkid) {
+		if (result[y >> 4] == null)
+		{
+			result[y >> 4] = new byte[4096];
+		}
+		result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blkid;
+	}
+
+	@SuppressWarnings("unused") // Yea yea eclipse I know it's not used but handy to keep
 	private int coordsToInt(int x, int y, int z) {
 		return (x * 64 + z) * 256 + y;
 	}
 
 	public double distance(int x, int y, int z)	{
-		return Math.sqrt(Math.pow(8 - x, 2.0D) + Math.pow(0 - y, 2.0D) + Math.pow(8 - z, 2.0D));
+		return Math.sqrt(Math.pow(8 - x, 2.0D) + Math.pow(8 - y, 2.0D) + Math.pow(8 - z, 2.0D));
 	}
 }
