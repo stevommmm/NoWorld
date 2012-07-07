0f8678ed332f6151431aad80388b1b7d6c9c0c47
Stephen Mcgregor <tehborris@gmail.com>
2012-07-07 14:50:47 +1000

    Fully working generation!!!!

--- src/com/c45y/NoWorld/NoWorldGen.java
+++ src/com/c45y/NoWorld/NoWorldGen.java
@@ -1,8 +1,8 @@
 package com.c45y.NoWorld;
 
 import java.util.Random;
+
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.World;
 
@@ -20,15 +20,19 @@ public class NoWorldGen extends ChunkGenerator
 
 	public byte[][] generateBlockSections(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomeGrid)
 	{
+		System.out.println("X: " + chunkX % 2 * 16 + " Z: " + chunkZ % 2 * 16);
 		byte[][] result = new byte[256 / 16][]; //world height / chunk part height (=16, look above)
-		for(int y = 0; y < 16; y++) {
-			for(int x = 0; x < 16; x++)
+		int x = 0, y = 0, z = 0;
+		this.i = (this.i % 4) + 1;
+		for(x = 0; x < 16; x++)
+		{
+			for(z = 0; z < 16; z++)
 			{
-				for(int z = 0; z < 16; z++)
-				{
-					if (distance(x,y,z) < 8) {
-						setBlock(result, x, y, z, (byte) Material.DIRT.getId());
+				for(y = 0; y < 16; y++) {
+					if (distance(chunkX % 2 * 16,chunkZ % 2 * 16 ,x ,y ,z) < 15) {
+						setBlock(result, x, y, z, (byte) this.i);
 					}
+
 				}
 			}
 		}
@@ -43,12 +47,13 @@ public class NoWorldGen extends ChunkGenerator
 		result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blkid;
 	}
 
-	@SuppressWarnings("unused") // Yea yea eclipse I know it's not used but handy to keep
 	private int coordsToInt(int x, int y, int z) {
 		return (x * 64 + z) * 256 + y;
 	}
 
-	public double distance(int x, int y, int z)	{
-		return Math.sqrt(Math.pow(8 - x, 2.0D) + Math.pow(8 - y, 2.0D) + Math.pow(8 - z, 2.0D));
+	public double distance(int sx, int sz, int x, int y, int z)	{
+		return Math.sqrt(Math.pow(Math.abs(sx) - x, 2.0D) + Math.pow(15 - y, 2.0D) + Math.pow(Math.abs(sz) - z, 2.0D));
 	}
+	
+	private int i = 0;
 }
